fa3898bd08c92a1855c5c8236ee94584fa3ef7ae
Davert <davert.php@resend.cc>
2014-09-03 03:08:39 +0300

    updated run command

--- src/Docker.php
+++ src/Docker.php
@@ -26,7 +26,7 @@ class DockerRunTask extends ExecTask
 
     public function getCommand()
     {
-        return trim('docker run ' . $this->arguments .' ' . $this->image . ' ' . $this->run);
+        return trim('docker run ' . $this->arguments .' ' . $this->image . ' "' . $this->run.'"');
     }
 
     public function interactive()
